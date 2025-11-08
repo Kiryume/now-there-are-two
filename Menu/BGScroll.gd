e44bfd5ac3498ce242566caa2dca7b2a55446f9e
Kirsten Pleskot <kirstenpleskot@gmail.com>
2025-11-08 21:22:47 +0100

    menu

---
+++ Menu/BGScroll.gd
@@ -0,0 +1,9 @@
+extends ColorRect
+
+@export var speed := 1.;
+
+@export var offset := 0.;
+
+func _process(delta: float) -> void:
+	offset += speed * delta
+	self.material.set("shader_parameter/offset", Vector2(0, offset))
