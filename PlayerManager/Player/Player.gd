0979447fce1f68adb45c80a977952b807b7e5e52
Kirsten Pleskot <kirstenpleskot@gmail.com>
2025-11-09 04:56:13 +0100

    player contact damage

--- PlayerManager/Player/Player.gd
+++ PlayerManager/Player/Player.gd
@@ -1,39 +1,43 @@
 extends CharacterBody2D
 class_name Player
 
-const speed := 250.
 
-@export
-var player_select := "left"
+@export var player_select := "left"
+@export var color_lerp_speed := 5.0
 
-@export
-var color_lerp_speed := 5.0
-
-@onready
-var text : ColorRect = $Texture
-
-@onready
-var muzzle_text : ColorRect = $WeaponController/MainNozzle/MuzzleTexture
-
-@onready
-var coin_pickup = $CoinPickup
+@onready var text : ColorRect = $Texture
+@onready var muzzle_text : ColorRect = $WeaponController/MainNozzle/MuzzleTexture
+@onready var coin_pickup = $CoinPickup
 
 var target_color: Color
+var health := 250.
+const MAX_HEALTH := 250.
 
+const speed := 250.
 const MAX_COLLISION_COUNT = 10
+
 
 func take_collision_dmg(enemy: Enemy):
 	pass
 	
 func on_projectile_collision(projectile: BaseProjectile):
 	pass
-
-
-func _process(delta: float) -> void:
+func update_color():
 	if player_select == "left":
 		target_color = Color.DEEP_PINK * 1.15
 	else:
 		target_color = Color.DEEP_SKY_BLUE * 1.15
+	target_color.s *= health / MAX_HEALTH
+
+func _ready() -> void:
+	update_color()
+	$Hurtbox.hurt.connect(_on_hurt)
+	
+func _on_hurt(damage_profile: DamageResource):
+	health -= damage_profile.amount
+	update_color() 
+
+func _process(delta: float) -> void:
 	text.color = text.color.lerp(target_color, delta * color_lerp_speed)
 	muzzle_text.color = muzzle_text.color.lerp(target_color, delta * color_lerp_speed)
 
